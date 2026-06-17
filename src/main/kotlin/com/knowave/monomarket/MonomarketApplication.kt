724f63b5bf15244d38a635f36bc8b2373a65c6da
knowave <kwaktaemin98@gmail.com>
2026-06-17 15:35:41 +0700

    FIRST COMMIT

---
+++ src/main/kotlin/com/knowave/monomarket/MonomarketApplication.kt
@@ -0,0 +1,11 @@
+package com.knowave.monomarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonomarketApplication
+
+fun main(args: Array<String>) {
+	runApplication<MonomarketApplication>(*args)
+}
---
+++ src/test/kotlin/com/knowave/monomarket/MonomarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.knowave.monomarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonomarketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
