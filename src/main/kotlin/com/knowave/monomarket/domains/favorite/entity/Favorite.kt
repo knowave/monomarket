87a6bfa1c1c4c86893a925f2942bbc6528edb42b
knowave <kwaktaemin98@gmail.com>
2026-06-19 13:28:35 +0700

    IN_PROGRESS: Favorite entity 정의

---
+++ src/main/kotlin/com/knowave/monomarket/domains/favorite/entity/Favorite.kt
@@ -0,0 +1,33 @@
+package com.knowave.monomarket.domains.favorite.entity
+
+import com.knowave.monomarket.common.entity.BaseEntity
+import com.knowave.monomarket.domains.product.entity.Product
+import com.knowave.monomarket.domains.user.entity.User
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
+
+@Entity
+@Table(
+    name = "favorites",
+    uniqueConstraints = [
+        UniqueConstraint(
+            name = "uk_favorite_user_product",
+            columnNames = ["user_id", "product_id"]
+
+        )
+    ]
+)
+
+class Favorite(
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    val product: Product,
+    ) : BaseEntity()
